4861eee2c4fb86820a211aeedaabc76bf97b79eb
gu-ramosp <gu.ramosp@gmail.com>
2016-11-27 23:28:30 -0200

    Funciona!!
    Mas o desempenho poderia ser melor, ele chega a bater nas paredes antes
    de desviar e às vezes fica girando

--- K-Junior+Fuzzy.py
+++ K-Junior+Fuzzy.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[431]:
+# In[ ]:
 
 ######################################################### Imports #############################################################
 
@@ -12,7 +12,7 @@ from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 
 
-# In[432]:
+# In[22]:
 
 ################################################## Conexão com o V-rep #######################################################
 
@@ -24,7 +24,7 @@ else:
     print("Erro na conexão")
 
 
-# In[433]:
+# In[ ]:
 
 ################################################ Declaração de Váriáveis ######################################################
    
@@ -33,12 +33,12 @@ else:
     
 ## Valores Crisp
 sensores_handle = []        #lista com os Handles dos sensores, para pegar os Handles mais fácil
-valores_sensores = []       # Lista com os valores dos sensores
+valores_sensores = [1,2,3,4,5]       # Lista com os valores dos sensores
 
 
 # Valores Fuzzy
     # Antecedentes: Váriaveis à serem Fuzificada
-ssensor_frente = ctrl.Antecedent(np.arange(0, 0.12, 0.01), 'sensor_frente')
+sensor_frente = ctrl.Antecedent(np.arange(0, 0.12, 0.01), 'sensor_frente')
 sensores_left  = ctrl.Antecedent(np.arange(0,0.12, 0.01), 'sensores_left')
 sensores_right = ctrl.Antecedent(np.arange(0, 0.12, 0.01), 'sensores_right')
 
@@ -47,28 +47,40 @@ v_left = ctrl.Consequent(np.arange(0, 10, 0.5 ), 'v_left') # velocidade motor es
 v_right = ctrl.Consequent(np.arange(0, 10, 0.5), 'v_right') # idem ao de cima pro motor direit
 
 
-# In[434]:
+# In[ ]:
 
 ############################################ Declaração de Handles ############################################################
 
 # Não sei direito o que é um handle até hoje, mas chutaria que ele é uma coisa que controla o objeto real
 
 #handle do motor esquerdo, chuto que o nome do objeto seja o mesmo da simulação
-erro_code, leftMotorHandle = vrep.simxGetObjectHandle(clientID,'KJunior_wheelLeft', vrep.simx_opmode_oneshot_wait )
-
+erro_code, leftMotorHandle = vrep.simxGetObjectHandle(clientID,'motorE', vrep.simx_opmode_oneshot_wait )
+if erro_code != 0:
+    print("handle nao encontrado")
+else:
+    print("Conectado ao motor esquerdo")
 #Handle do motor Direito
-erro_code, rightMotorHandle = vrep.simxGetObjectHandle(clientID,'KJunior_wheelLeft', vrep.simx_opmode_oneshot_wait )
-
+erro_code, rightMotorHandle = vrep.simxGetObjectHandle(clientID,'motorD', vrep.simx_opmode_oneshot_wait )
+if erro_code != 0:
+    print("handle nao encontrado")
+else:
+    print("Conectado ao motor  direito")
+    
 #Handle dos sensores, só importam os do 1 ao 5, o 3 é o sensor mais centralizado
 for i in range(5):
     erro_code, sensor_handle = vrep.simxGetObjectHandle(clientID,'KJunior_proxSensor%d' % (i+1) , vrep.simx_opmode_oneshot_wait)
     sensores_handle.append(sensor_handle)
-
+    if erro_code != 0:
+        print("handle do sensor %d não encontrado" %(i+1) )
+    else:
+        print("Conectado ao sensor %d " %(i+1) )
+    
     # Não sei porque, mas a primeira leitura tem que ser assim
     error_code,estado,coord,detectedObjectHandle,detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID, sensor_handle,vrep.simx_opmode_streaming)
+    
 
 
-# In[435]:
+# In[ ]:
 
 ############################################# Lógica Fuzzy ################################################################
 
@@ -112,89 +124,89 @@ rule6 = ctrl.Rule(sensor_frente['perto'] & sensores_left['perto'] & sensores_rig
 # ## Gráficos das funções de pertinência 
 # 
 
-# In[436]:
+# In[ ]:
 
 ############################# INPUTS ################################
 
-get_ipython().magic('matplotlib inline')
-sensor_frente.view()
-sensores_left.view()
-sensores_right.view()
+#get_ipython().magic('matplotlib inline')
+#sensor_frente.view()
+#sensores_left.view()
+#sensores_right.view()
 
 
-# In[437]:
+# In[ ]:
 
 ####################################### Outputs ####################################################
-get_ipython().magic('matplotlib inline')
-v_left.view()
-v_right.view()
-
-
-# ################################# controlando o Robô ##############################################################
-# 
-# while vrep.simxGetConnectionId(clientID != -1): #Enquanto houver conexão com o V-rep
-#     
-#     #Lê os sensores do robo e os coloca na lista de valores
-#     for i in range(5):
-#         error_code, estado, coord, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensores_handle[i], vrep.simx_opmode_buffer)
-#         # Não sei pra que serve o estado, nem os dois últimos parametros retornados
-#         valores_sensores.append(coord[2]) # pega apenas cordenada Z que é a que importe de verdade
-#         
-#         ### Sistema de controle fuzzy
-#         fuzzy_ctrl = ctrl.ControlSystem([rule1, rule2, rule3,rule4,rule5,rule6]) #sistema de controle
-#         kjunior = ctrl.ControlSystemSimulation(fuzzy_ctrl) #simulação do sistema de controle, pode-se criar vários para cada situação
-# 
-# 
-#         #Inputs para a simulação do K-junior
-#         kjunior.input['sensor_frente'] = 0.08
-#         kjunior.input['sensores_left'] = 0.02
-#         kjunior.input['sensores_right'] = 0.1
-# 
-#         ### Processando a simulação do controle
-#         kjunior.compute()
-# 
-#         vl = kjunior.output['v_left']         #Pega velocidade a ser passada para o motor direito
-#         vr = kjunior.output['v_right']        #Pega velocidade a ser passada para o motor esquerdo
-#     
-#     # Passando as velocidades para o K-junior
-#     erro_code = vrep.simxSetJointTargetVelocity(clientID, leftMotorHandle, vl, vrep.simx_opmode_streaming)
-#     erro_code = vrep.simxSetJointTargetVelocity(clientID, leftMotorHandle, vr, vrep.simx_opmode_streaming)
-#     
-#    
-#     
-
-# In[438]:
-
-# Teste apenas
-
-### Sistema de controle fuzzy
-fuzzy_ctrl = ctrl.ControlSystem([rule1, rule2, rule3,rule4,rule5,rule6]) #sistema de controle
-kjunior = ctrl.ControlSystemSimulation(fuzzy_ctrl) #simulação do sistema de controle, pode-se criar vários para cada situação
-
-
-#Inputs para a simulação do K-junior
-kjunior.input['sensor_frente'] = 0.08
-kjunior.input['sensores_left'] = 0.02
-kjunior.input['sensores_right'] = 0.1
-
-### Processando a simulação do controle
-kjunior.compute()
-
-vl = kjunior.output['v_left']         #Pega velocidade a ser passada para o motor direito
-vr = kjunior.output['v_right']        #Pega velocidade a ser passada para o motor esquerdo
-
-print (kjunior.output)
+#get_ipython().magic('matplotlib inline')
+#v_left.view()
+#v_right.view()
 
 
 # In[ ]:
 
+################################# controlando o Robô ##############################################################
+
+while vrep.simxGetConnectionId(clientID != -1): #Enquanto houver conexão com o V-rep
+    
+    #Lê os sensores do robo e os coloca na lista de valores
+    for i in range(5):
+        error_code, estado, coord, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensores_handle[i], vrep.simx_opmode_buffer)
+        # Não sei pra que serve o estado, nem os dois últimos parametros retornados
+        valores_sensores[i] = coord[2] # pega apenas cordenada Z que é a que importe de verdade
+        
+        ### Sistema de controle fuzzy
+        fuzzy_ctrl = ctrl.ControlSystem([rule1, rule2, rule3,rule4,rule5,rule6]) #sistema de controle
+        kjunior = ctrl.ControlSystemSimulation(fuzzy_ctrl) #simulação do sistema de controle, pode-se criar vários para cada situação
 
 
+        #Inputs para a simulação do K-junior
+        kjunior.input['sensor_frente'] = (valores_sensores[0] + valores_sensores[1])/2 #media dos valores 
+        kjunior.input['sensores_left'] = valores_sensores[2]
+        kjunior.input['sensores_right'] = (valores_sensores[3] + valores_sensores[4])/2 # media dos valores
 
-# In[ ]:
+        ### Processando a simulação do controle
+        kjunior.compute()
+
+        vl = kjunior.output['v_left']         #Pega velocidade a ser passada para o motor direito
+        vr = kjunior.output['v_right']        #Pega velocidade a ser passada para o motor esquerdo
+    
+    # Passando as velocidades para o K-junior
+    
+    #Motor Esquerdo
+    erro_code = vrep.simxSetJointTargetVelocity(clientID, leftMotorHandle, vl, vrep.simx_opmode_streaming)
+    if erro_code !=0:
+        print("erro na atribuição de velocidade do motor esquerdo")
+    
+    #Motor Direito
+    erro_code = vrep.simxSetJointTargetVelocity(clientID, rightMotorHandle, vr, vrep.simx_opmode_streaming)
+    if erro_code !=0:
+       print("erro na atribuição de velocidade do motor direito")
+ 
+    
 
 
-
+# # Teste apenas
+# 
+# Sistema de controle fuzzy
+# 
+# fuzzy_ctrl = ctrl.ControlSystem([rule1, rule2, rule3,rule4,rule5,rule6]) #sistema de controle
+# kjunior = ctrl.ControlSystemSimulation(fuzzy_ctrl) #simulação do sistema de controle, pode-se criar vários para cada situação
+# 
+# 
+# Inputs para a simulação do K-junior
+# 
+# kjunior.input['sensor_frente'] = 0.08
+# kjunior.input['sensores_left'] = 0.02
+# kjunior.input['sensores_right'] = 0.1
+# 
+# Processando a simulação do controle
+# 
+# kjunior.compute()
+# 
+# vl = kjunior.output['v_left']         #Pega velocidade a ser passada para o motor direito
+# vr = kjunior.output['v_right']        #Pega velocidade a ser passada para o motor esquerdo
+# 
+# print (kjunior.output)
 
 # In[ ]:
 
